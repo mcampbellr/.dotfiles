3b0275e2ebaff4a05f744afd62373c6a9514900d
Mario Campbell R <mariocampbellr@gmail.com>
2022-11-04 02:00:43 -0600

    chore(nvim): new config core

--- nvim/.config/nvim/lua/mcampbellr/colors.lua
+++
@@ -1,4 +0,0 @@
-return {
-  Theme = 'neosolarized',
-  Lualine = 'gruvbox'
-}
--- nvim/.config/nvim/lua/mcampbellr/init.lua
+++ nvim/.config/nvim/lua/mcampbellr/init.lua
@@ -1,8 +1,9 @@
-require "mcampbellr.sets"
+require "mcampbellr.core.sets"
+require "mcampbellr.core.colorscheme"
+require "mcampbellr.core.commands"
+
 require "mcampbellr.plugins"
-require "mcampbellr.colorscheme"
 require "mcampbellr.cmp"
-require "mcampbellr.commands"
 require "mcampbellr.lsp"
 require "mcampbellr.debugger"
 require "mcampbellr.autocommands"
