0a889b586067a08d7740e8c14596809628fad296
Mario Campbell R <mcampbellrojas@tevora.com>
2023-06-23 13:19:18 -0600

    feat: added comments and github copilot

--- nvim/.config/nvim/lua/mcampbellr/plugins/copilot.lua
+++ nvim/.config/nvim/lua/mcampbellr/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
     "github/copilot.vim",
     init = function() 
-        vim.g.copilot_no_tab_map = true
+        --vim.g.copilot_no_tab_map = true
         vim.g.copilot_node_command = "~/.config/nvm/versions/node/v18.16.0/bin/node"
     end
 }
