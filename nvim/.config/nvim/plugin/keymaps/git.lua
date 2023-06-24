e42e889b7ee69d1ef65374b485f38b444b619fcc
Mario Campbell R <mcampbellrojas@tevora.com>
2023-06-23 20:33:38 -0600

    chore: some cleanup

--- nvim/.config/nvim/plugin/keymaps/git.lua
+++
@@ -1,9 +0,0 @@
-local Remap = require "mcampbellr.keymap"
-local nnoremap = Remap.nnoremap
-
-nnoremap("g[", ":Gitsigns next_hunk<CR>")
-nnoremap("g]", ":Gitsigns prev_hunk<CR>")
-nnoremap("<leader>gb", ":Gitsigns blame_line<CR>")
-
--- File history
---[[ nnoremap("<leader>fh", ":GitTimeLapse <CR>") ]]
